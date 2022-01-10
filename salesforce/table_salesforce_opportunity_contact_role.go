af3ce2675501c98621f64b0deaf92291dbbd0e1e
LalitTurbot <lalit@turbot.com>
2022-01-10 17:09:01 +0530

    update

--- salesforce/table_salesforce_opportunity_contact_role.go
+++ salesforce/table_salesforce_opportunity_contact_role.go
@@ -11,7 +11,7 @@ import (
 func SalesforceOpportunityContactRole(_ context.Context) *plugin.Table {
 	return &plugin.Table{
 		Name:        "salesforce_opportunity_contact_role",
-		Description: "salesforce Opportunity Contact Role",
+		Description: "Represents the role that a Contact plays on an Opportunity.",
 		List: &plugin.ListConfig{
 			Hydrate: listSalesforceOpportunityContactRole,
 		},
@@ -21,10 +21,10 @@ func SalesforceOpportunityContactRole(_ context.Context) *plugin.Table {
 		},
 		Columns: []*plugin.Column{
 			{Name: "id", Type: proto.ColumnType_STRING, Description: "Unique identifier of the opportunity contact role in Salesforce."},
-			{Name: "contact_id", Type: proto.ColumnType_STRING, Description: "The id of the contact or person account."},
-			{Name: "is_primary", Type: proto.ColumnType_BOOL, Description: "Indicates, the person as the primary contact for the opportunity."},
-			{Name: "opportunity_id", Type: proto.ColumnType_STRING, Description: "Id of the opportunity that's associated with the opportunity contact role."},
-			{Name: "role", Type: proto.ColumnType_STRING, Description: "The role of the contact for the record."},
+			{Name: "contact_id", Type: proto.ColumnType_STRING, Description: "ID of an associated Contact."},
+			{Name: "is_primary", Type: proto.ColumnType_BOOL, Description: "Indicates whether the associated Contact plays the primary role on the Opportunity (true) or not (false)."},
+			{Name: "opportunity_id", Type: proto.ColumnType_STRING, Description: "Required. ID of an associated Opportunity."},
+			{Name: "role", Type: proto.ColumnType_STRING, Description: "Name of the role played by the associated Contact on the Opportunity, such as Business User or Decision Maker."},
 
 			{Name: "created_by_id", Type: proto.ColumnType_STRING, Description: "Id of the user who created contact role record."},
 			{Name: "created_date", Type: proto.ColumnType_TIMESTAMP, Description: "Date and time of the creation of the contact role record."},
